6bdeacd8d88b51ed57c194fa4e67603e2b24265c
saahir <33964080+saahirlol@users.noreply.github.com>
2024-08-04 21:20:16 +0000

    ##rebase ##fuckcrashes

---
+++ Dockerfile
@@ -0,0 +1,43 @@
+ARG TSVERSION=1.70.0
+ARG TSFILE=tailscale_${TSVERSION}_amd64.tgz
+FROM alpine:latest as build
+ARG TSFILE
+WORKDIR /app
+
+RUN wget https://pkgs.tailscale.com/stable/${TSFILE} && \
+  tar xzf ${TSFILE} --strip-components=1
+COPY . ./
+
+
+FROM alpine:latest
+RUN apk update \
+    && apk add --no-cache \
+      ca-certificates \
+      iptables \
+      ip6tables \
+      iproute2 \
+      squid \
+      dante-server \
+      python3 \
+      dnsmasq \
+      caddy \
+    && rm -rf /var/cache/apk/*
+RUN mkdir -p \
+      /var/run/tailscale \
+      /var/cache/tailscale \
+      /var/lib/tailscale \
+      /etc/squid/
+
+# Copy binary to production image
+COPY --from=build /app/start.sh /app/start.sh
+COPY --from=build /app/tailscaled /app/tailscaled
+COPY --from=build /app/tailscale /app/tailscale
+COPY --from=build /app/motd /etc/motd
+COPY --from=build /app/sockd.conf /etc/sockd.conf
+COPY --from=build /app/squid.conf /etc/squid/squid.conf
+COPY --from=build /app/dnsmasq.conf /etc/dnsmasq.conf
+
+# Run on container startup.
+USER root
+ENV HS="https://controlplane.tailscale.com"
+CMD ["/app/start.sh"]
---
+++ start.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+echo 'Starting up Tailscale...'
+
+# error: adding [-i tailscale0 -j MARK --set-mark 0x40000] in v4/filter/ts-forward: running [/sbin/iptables -t filter -A ts-forward -i tailscale0 -j MARK --set-mark 0x40000 --wait]: exit status 2: iptables v1.8.6 (legacy): unknown option "--set-mark"
+modprobe xt_mark
+
+echo 'net.ipv4.ip_forward = 1' | tee -a /etc/sysctl.conf
+echo 'net.ipv6.conf.all.forwarding = 1' | tee -a /etc/sysctl.conf
+#echo 'net.ipv6.conf.all.disable_policy = 1' | tee -a /etc/sysctl.conf
+sysctl -p /etc/sysctl.conf
+
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+ip6tables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+
+
+
+# Start tailscaled
+/app/tailscaled --verbose=1 --port 41641 &
+sleep 5
+
+if [ ! -S /var/run/tailscale/tailscaled.sock ]; then
+    echo "tailscaled.sock does not exist. exit!"
+    exit 1
+fi
+
+until /app/tailscale up \
+    --login-server=${HS} \
+    --authkey=${TAILSCALE_AUTH_KEY} \
+    --hostname=flyio-${HOSTNAME} \
+    --tun=userspace-networking \
+    --state=/var/lib/tailscale/  \
+do
+    sleep 0.1
+done
+
+echo 'Tailscale started'
+
+echo 'Starting Squid...'
+
+squid &
+
+echo 'Squid started'
+
+echo 'Starting Dante...'
+
+sockd &
+
+echo 'Dante started'
+
+echo 'Starting dnsmasq...'
+
+dnsmasq &
+
+echo 'Dnsmasq started'
+
+echo 'Starting caddy...'
+
+caddy run --config /etc/caddy/Caddyfile --adapter caddyfile  &
+
+echo 'Caddy started'
+
+sleep infinity
